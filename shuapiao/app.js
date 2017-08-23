eaec46da71aceada549fe2896a695fc33b92dab4
zhouxinjian52 <zhouxinjian52@126.com>
2017-08-23 18:20:16 +0800

    shuapiao new file

--- shuapiao/app.js
+++ shuapiao/app.js
@@ -36,7 +36,7 @@ const options = {
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         "Host": "www.xinyegou.cn",
         "Origin": "http://www.xinyegou.cn",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_3 like Mac OS X) AppleWebKit/603.3.8 (KHTML, like Gecko) Mobile/14G60 MicroMessenger/6.5.14 NetType/WIFI Language/zh_CN",
         "X-Requested-With": "XMLHttpRequest",
         "Host": urlParsed.hostname,
         "Proxy-Authorization": "Basic " + base64
@@ -51,6 +51,46 @@ function sleep(numberMillis) {
         if (now.getTime() > exitTime)
             return;
     }
+}
+
+function spider() {
+    options.headers['Cookie'] = "PHPSESSID=o4i2qmhs9ekjpvk3q9por2sn22; wxd_openid=Eioa5C5oj3S32qhH; dzp_openid=Eioa5C5oj3S32qhH"
+
+    // console.log("got response: " + res.statusCode);
+    // console.log("body:" + body);
+    const postData = querystring.stringify({
+        zid: 149,
+        vid: 13,
+        token: 'Eioa5C5oj3S32qhH',
+        // __hash__: $('input[name="__hash__"]').val(),
+        // tttid: $('input[name="tttid"]').val()
+    });
+
+    const req = http.request(options, function(res) {
+        console.log("got response: " + res.statusCode);
+        console.log(res.headers['set-cookie']);
+        res.on('data', function(body) {
+            console.error("body:" + body);
+            if (body == 108) {
+                SUCCESS++;
+            }
+            console.error('成功刷了：' + SUCCESS + '次')
+            sleep(Math.ceil(Math.random()*1000));
+            if (body == 107) {
+                sleep(Math.ceil(Math.random()*15000));
+            }
+            spiderSet();
+        })
+    }).on('error', function(err) {
+        console.error("err:" + err);
+        console.log('error');
+        sleep(1000);
+        spiderSet();
+    })
+
+    // 写入数据到请求主体
+    req.write(postData);
+    req.end();
 }
 
 function spiderSet(req, res) {
@@ -86,7 +126,7 @@ function spiderSet(req, res) {
 
             const req = http.request(options, function(res) {
                 console.log("got response: " + res.statusCode);
-                console.log( res.headers['set-cookie']);
+                console.log(res.headers['set-cookie']);
                 res.on('data', function(body) {
                     console.error("body:" + body);
                     if (body == 108) {
@@ -96,33 +136,39 @@ function spiderSet(req, res) {
                     sleep(100);
                     spiderSet();
                 })
-            });
+            }).on('error', function(err) {
+                console.error("err:" + err);
+                console.log('error');
+                sleep(100);
+                spiderSet();
+            })
 
             // 写入数据到请求主体
             req.write(postData);
             req.end();
         })
 }
-spiderSet();
-// var cluster = require('cluster');
-// var numCPUs = 3;
+// spiderSet();
+var cluster = require('cluster');
+var numCPUs = 5;
 
-// console.time('3 cluster');
-// if (cluster.isMaster) {
-//     console.log(111111111);
-//     // Fork workers.
-//     for (var i = 0; i < numCPUs; i++) {
-//         cluster.fork();
-//     }
-//     var i = numCPUs;
-//     cluster.on('exit', function(worker, code, signal) {
-//         if (!--i) {
-//             console.timeEnd('3 cluster');
-//             process.exit(0);
-//         }
-//     });
-// } else {
-//     console.log(22222222222);
-//     spiderSet();
-//     // process.exit(0);
-// }
+console.time('5 cluster');
+if (cluster.isMaster) {
+    console.log(111111111);
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        sleep(Math.ceil(Math.random()*1000)+300);
+        cluster.fork();
+    }
+    var i = numCPUs;
+    cluster.on('exit', function(worker, code, signal) {
+        if (!--i) {
+            console.timeEnd('5 cluster');
+            process.exit(0);
+        }
+    });
+} else {
+    console.log(22222222222);
+    spider();
+    // process.exit(0);
+}
